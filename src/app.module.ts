f9484d00ae9caf048ab50cf101577bc448b383ea
denizyoldas <daksu30@gmail.com>
2021-08-22 14:52:11 +0300

    add new auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MenuModule } from './menu/menu.module';
 import { SettingsModule } from './settings/settings.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), MenuModule, SettingsModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    MenuModule,
+    SettingsModule,
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
